1bb03b1249573ff3c5c23cec8bbcc9294adcb40d
Ozan Yücel <68647366+ozanyucell@users.noreply.github.com>
2021-12-13 00:05:26 +0300

    v0.8 :)
    Well, this is the first full functional prototype.

--- viewer.py
+++ viewer.py
@@ -1,23 +1,28 @@
 import os
-import time
-content_displayed="There is no message."
+import tkinter
 
-print(content_displayed)
+content_displayed = "There is no message."
 
-while True:
+root = tkinter.Tk()
+root.attributes('-fullscreen', True)
+root.title("Displayer")
+
+v = tkinter.StringVar()
+v.set("There is no message.")
+
+label = tkinter.Label(root, textvariable=v, justify="center", fg="white", height=72, width=274, bg="black", font=("calibri", 64))
+label.pack()
+
+def content_switcher():
     with open(f"{os.getcwd()}/web_service/depo.txt", mode='r', encoding='utf-8') as f:
-        content = f.read()
-        if (content == ""):
-            content == "There is no message."
+        content_displayed = f.read()
 
-    if(content==content_displayed):
+        if content_displayed == "":
+            content_displayed == "There is no message."
+
         f.close()
-        time.sleep(5)
-        continue
+        v.set(content_displayed)
+        root.after(1000, content_switcher)
 
-    else:
-        content_displayed = content
-
-    print(content_displayed)
-    f.close()
-    time.sleep(5)
+root.after(0, content_switcher)
+root.mainloop()
